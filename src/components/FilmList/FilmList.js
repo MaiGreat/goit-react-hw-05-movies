e22df6edd16a0e8f3cc3a08b1b3499d3a064f23f
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-07-06 21:14:01 +0200

    defaultImg

--- src/components/FilmList/FilmList.js
+++ src/components/FilmList/FilmList.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import css from './filmList.module.css'
+import defaultImg  from '../../imgs/picture-coming-soon.webp'
 
 const FilmList = ({ movies }) => {
     return (
@@ -9,8 +10,10 @@ const FilmList = ({ movies }) => {
             {movies.map(({ id, title, poster_path, tagline }) => (
                 <li className={css['list-film']} key={id}>
                     <img className={css.img }
-                        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                        src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : defaultImg}
                         alt={tagline ? tagline : "Poster"}
+                        width={250}
+                        height={360}
                     />
                     <Link className={css.title} to={`/detailsFilm/${id}`}>{title}</Link>
                 </li>
--- src/components/service/API.js
+++ src/components/service/API.js
@@ -6,7 +6,6 @@ const API_KEY = '927b7735e8c7db5d43dd9f92192df314';
 
 export const getTrendingMovies = async () => {
     const { data } = await axios.get(`trending/movie/week?api_key=${API_KEY}`);
-    console.log(data);
     return data.results;
 };
 
@@ -15,7 +14,6 @@ export const getDetalisMovies = async id => {
     const { data } = await axios.get(
         `movie/${id}?api_key=${API_KEY}&language=en-US`
     );
-    console.log(data);
     return data;
 };
 
@@ -23,7 +21,6 @@ export const getCastMovies = async id => {
         const { data } = await axios.get(
         `movie/${id}/credits?api_key=${API_KEY}&language=en-US`
     );
-    console.log(data);
     return data;
 }
 
@@ -31,7 +28,6 @@ export const getRewiewsMovies = async id => {
     const { data } = await axios.get(
         `movie/${id}/reviews?api_key=${API_KEY}&language=en-US`
     )
-    console.log(data);
     return data;
 }
 
